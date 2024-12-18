dbffdfc0cbcb377c61a24bad803ce0d5a8a483ab
narayanakilari <narayana.ds93@gjmail.com>
2024-12-18 17:28:04 +0530

    To DO Gui initial program

--- ToDo_App/gui.py
+++ ToDo_App/gui.py
@@ -6,9 +6,10 @@ label=sg.Text("Type in a to-do")
 input_box=sg.InputText()
 add_button=sg.Button("Add")
 
-window=sg.Window("My To DO App",layout=[[label],[input_box,add_button]])
+window=sg.Window("My To Do App",layout=[[label],[input_box,add_button]])
 window.read()
 window.close()
+
 
 
 
